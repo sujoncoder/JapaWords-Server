5775a56153346f9a151b0b0c71791bdbd80bea1a
sujoncoder <sujonsheikh.in@gmail.com>
2024-12-14 15:10:49 +0600

    admin change

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -6,8 +6,8 @@ import { isAdmin, isLoggedin } from "../middleware/auth.js";
 // EXTRACT AUTH ROUTER FROM EXPRESS
 const userRouter = express.Router();
 
-userRouter.get("/", isLoggedin, isAdmin, getAllUser);
-userRouter.get("/:id([0-9a-fA-F]{24})", isLoggedin, isAdmin, getSingleUser);
+userRouter.get("/", isLoggedin, getAllUser);
+userRouter.get("/:id([0-9a-fA-F]{24})", isLoggedin, getSingleUser);
 
 
 export default userRouter;

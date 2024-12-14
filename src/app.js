6d36ab08220d2c727e7285490dd50246d4ff44a6
sujoncoder <sujonsheikh.in@gmail.com>
2024-12-14 15:08:59 +0600

    trying to deploy vercel

--- src/app.js
+++ src/app.js
@@ -17,14 +17,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
-    Credential: true
+    origin: "*",
+    credentials: true
 }));
+
 
 
 // ROOT ROUTE
 app.get("/", (req, res) => {
     res.status(200).send("Yes server is working.")
+});
+
+app.get("/hello", (req, res) => {
+    res.status(200).send("Hello developer. How are you.")
+});
+
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`);
+    next();
 });
 
 // APPLICATION ROUTE
--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import app from "./app.js";
-import connectDB from "./config/db.js"
-import { PORT } from "./config/constants.js";
+import connectDB from "./config/db.js";
 
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, async () => {
-    console.log(`Srver is running on ${PORT}`);
+    console.log(`Server is running on ${PORT}`);
     await connectDB();
 });
